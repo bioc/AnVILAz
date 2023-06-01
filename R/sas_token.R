7291dbb5bc90cfa8b7f2f57d7e49f3d0390eda05
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-05-31 21:07:40 -0400

    use workspace id helpers

--- R/sas_token.R
+++ R/sas_token.R
@@ -1,9 +1,12 @@
 .get_sas_token <- function() {
     sas_tkn <- POST(
         url = paste0(
-            .DSDE_PROD_URL, "api/workspaces/v1/", WSID,
+            .DSDE_PROD_URL,
+            "api/workspaces/v1/",
+            .workspace_id(),
             "/resources/controlled/azure/storageContainer/",
-            WSCID, "/getSasToken"
+            .workspace_storage_cont_id(),
+            "/getSasToken"
         ),
         query = list(sasExpirationDuration=28800),
         add_headers(
@@ -12,4 +15,3 @@
     )
     content(sas_tkn)
 }
-
