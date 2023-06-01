6becaaba54e3653f6ee47de34a64aa9e08e1c236
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-06-01 10:36:41 -0400

    paste token to path

--- R/az_copy.R
+++ R/az_copy.R
@@ -2,8 +2,10 @@ az_copy <- function(from, to) {
     sas_cred <- .get_sas_token()
     tkn <- sas_cred[["token"]]
     path <- shQuote(sas_cred[["url"]])
-    if (!missing(to))
-        path <- file.path(.workspace_storage_cont_url(), to)
+    if (!missing(to)) {
+        path <- file.path(.workspace_storage_cont_url(), to, "?")
+        path <- paste0(path, tkn)
+    }
     cmd <- "azcopy"
     args <- c("copy", from, path)
     system2(command = cmd, args = args, stdout = TRUE)
