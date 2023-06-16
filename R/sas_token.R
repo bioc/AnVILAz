a6c31f75423f490121664e2976aa8d21b4dd26c1
LiNk-NY <marcel.ramosperez@roswellpark.org>
2023-06-16 15:18:07 +0000

    document get_sas_token

--- R/sas_token.R
+++ R/sas_token.R
@@ -1,6 +1,25 @@
+#' Obtain the Shared Access Signature (SAS) for the Azure Storage
+#'
+#' The function provides a user delegation SAS token for management of
+#' resources. Mainly used in other functions to move files to and from the
+#' Azure Storage Container
+#'
+#' @inheritParams httr::content
+#'
+#' @param sasExpirationDuration `numeric(1)` The number of seconds until the SAS
+#'   token expires (default: 28,800 seconds)
+#'
+#' @return A list of two elements named `token` and `url`
+#'
+#' @examples
+#' if (interactive()) {
+#'   sas <- get_sas_token()
+#'   sas[["token"]]
+#'   sas[["url"]]
+#' }
 #' @importFrom httr POST
 #' @export
-get_sas_token <- function(as = NULL, expiration = 28800) {
+get_sas_token <- function(as = "parsed", sasExpirationDuration = 28800) {
     workspaceId <- workspace_id()
     resourceId <- workspace_storage_cont_id()
     api_endpoint <- paste0(
@@ -11,7 +30,7 @@ get_sas_token <- function(as = NULL, expiration = 28800) {
     url <- paste0(.DSDE_PROD_URL, endpoint)
     sas_tkn <- POST(
         url = url,
-        query = list(sasExpirationDuration = expiration),
+        query = list(sasExpirationDuration = sasExpirationDuration),
         add_headers(
             authorization = az_token()
         )
