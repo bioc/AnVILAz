381c4fc778f9f48bfc787f0baa04d1e736c67448
LiNk-NY <marcel.ramos@roswellpark.org>
2023-11-30 15:40:46 -0500

    make az_token internal

--- R/authentication.R
+++ R/authentication.R
@@ -1,4 +1,15 @@
-#' @export
+#' @title Generate an Azure authentication token with the `az` command line
+#'   utility
+#'
+#' @description This function generates an Azure authentication token with the
+#'   `az` command line utility. The token is used to authenticate with the Azure
+#'   services. This function is called internally by the `az_*` functions. It
+#'   is not meant to be called directly.
+#'
+#' @return A character string containing the authentication token with a
+#'   "Bearer" prefix.
+#'
+#' @keywords internal
 az_token <- function() {
     system2("az", c("login",  "--identity", "--allow-no-subscriptions"))
     cli_token <- system2("az", c("account", "get-access-token"), stdout = TRUE)
