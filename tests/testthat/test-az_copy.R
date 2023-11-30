ae5c8972c9edb0023f1a5a1f10f4b68e631a3d94
LiNk-NY <marcel.ramos@roswellpark.org>
2023-11-30 10:12:56 -0500

    update unit tests

--- tests/testthat/test-az_copy.R
+++ tests/testthat/test-az_copy.R
@@ -25,5 +25,70 @@ test_that("copying files works", {
     )
 
     az_copy_rm("analyses/test/test.log")
+    expect_false(
+        any(
+            grepl("test\\.log$", az_copy_list()[["INFO"]])
+        )
+    )
+    az_copy_rm("analyses/test")
+    expect_false(
+        any(
+            grepl("test", az_copy_list()[["INFO"]])
+        )
+    )
+
     file.remove("test.log")
+})
+
+test_that("backing up and restoring works", {
+    .skip_if_not_anvilaz()
+    dir.create("test")
+    file.create("test/test.log")
+
+    ## create remote folder and copy file
+    az_copy_backup("./test", "analyses/test_backup", contentsOnly = TRUE)
+    expect_true(
+        any(
+            grepl("test_backup\\/test\\.log$", az_copy_list()[["INFO"]])
+        )
+    )
+
+    az_copy_rm("analyses/test_backup", recursive = TRUE)
+    expect_false(
+        any(
+            grepl("test\\.log$", az_copy_list()[["INFO"]])
+        )
+    )
+
+    az_copy_backup("./test", "analyses/test_backup")
+    expect_true(
+        any(
+            grepl("test_backup\\/test\\/test\\.log$", az_copy_list()[["INFO"]])
+        )
+    )
+    az_copy_rm("analyses/test_backup", recursive = TRUE)
+
+    az_copy_backup("./test", "analyses/test_restore", contentsOnly = TRUE)
+    az_copy_restore(
+        "analyses/test_restore", "./test_restore", contentsOnly = TRUE
+    )
+    expect_true(
+        dir.exists("test_restore/")
+    )
+    expect_true(
+        file.exists("test_restore/test.log")
+    )
+
+    unlink("./test_restore", recursive = TRUE)
+
+    az_copy_restore("analyses/test_restore", "./test_restore")
+    expect_true(
+        dir.exists("test_restore/test_restore")
+    )
+    expect_true(
+        file.exists("test_restore/test_restore/test.log")
+    )
+
+    unlink("./test_restore", recursive = TRUE)
+    az_copy_rm("analyses/test_restore", recursive = TRUE)
 })
