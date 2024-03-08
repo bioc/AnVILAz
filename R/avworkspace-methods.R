1dd273dd673e7d9901f7bcca588f7cc23eae176c
LiNk-NY <marcel.ramosperez@roswellpark.org>
2024-03-08 17:02:01 -0500

    add avworkspace_clone method

--- R/avworkspace-methods.R
+++ R/avworkspace-methods.R
@@ -77,3 +77,51 @@ setMethod("avworkspace", signature = c(platform = "azure"), definition =
         paste0(avworkspace_namespace(), "/", avworkspace_name())
     }
 )
+
+# avworkspace_clone -------------------------------------------------------
+
+#' @describeIn avworkspace-methods Clone a workspace
+#'
+#' @importFrom AnVILBase avworkspace_clone
+#' @exportMethod avworkspace_clone
+setMethod("avworkspace_clone", signature = c(platform = "azure"),
+    definition = function(
+        namespace = avworkspace_namespace(),
+        name = avworkspace_name(),
+        to_namespace = namespace,
+        to_name,
+        bucket_location = "US",
+        ..., platform = cloud_platform()
+    ) {
+        stopifnot(
+            isScalarCharacter(to_name),
+            isScalarCharacter(to_namespace),
+            isScalarCharacter(bucket_location),
+            `source and destination 'namespace/name' must be different` =
+                !identical(namespace, to_namespace) || !identical(name, to_name)
+        )
+
+        api_endpoint <-
+            "/api/workspaces/{{workspaceNamespace}}/{{workspaceName}}/clone"
+        url <- paste0(.RAWLS_URL, api_endpoint)
+        url <- whisker.render(url)
+
+        request(url) |>
+            req_auth_bearer_token(az_token()) |>
+            req_body_json(
+                list(
+                    namespace = to_namespace,
+                    name = to_name,
+                    authorizationDomain = list(), # []
+                    attributes = structure(list(), .Names = character()), # {}
+                    copyFilesWithPrefix = "analyses/",
+                    # createdBy = userName,
+                    bucketLocation = bucket_location,
+                    enhancedBucketLogging = FALSE
+                )
+            ) |>
+            req_perform()
+
+        paste(to_namespace, to_name, sep = "/")
+    }
+)
