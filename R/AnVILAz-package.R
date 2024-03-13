93bc3b7e94fd32be4861e3189bf6a95369e06de2
Marcel Ramos <LiNk-NY@users.noreply.github.com>
2024-03-13 18:00:11 -0400

    add avdata methods
    - avdata and avdata_import

--- R/AnVILAz-package.R
+++ R/AnVILAz-package.R
@@ -4,7 +4,7 @@
 ## usethis namespace: start
 #' @importFrom httr2 request
 #'   req_auth_bearer_token req_headers req_cache req_perform req_method
-#'   req_body_multipart req_body_json
+#'   req_template req_body_multipart req_body_json
 #'   resp_is_error req_url_query
 #'   resp_body_string resp_body_json
 ## usethis namespace: end
