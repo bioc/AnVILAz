a669d0aade3e131ac497df0e243a8e51e76f0044
LiNk-NY <marcel.ramos@sph.cuny.edu>
2026-05-22 17:21:49 -0400

    add importFrom tags for roxygen2

--- R/AnVILAz-package.R
+++ R/AnVILAz-package.R
@@ -3,9 +3,9 @@
 
 ## usethis namespace: start
 #' @importFrom httr2 request
-#'   req_auth_bearer_token req_headers req_cache req_perform req_method
-#'   req_template req_body_multipart req_body_json
-#'   resp_is_error req_url_query
-#'   resp_body_string resp_body_json
+#' @importFrom httr2 req_auth_bearer_token req_headers req_cache req_perform
+#' @importFrom httr2 req_method req_template req_body_multipart req_body_json
+#' @importFrom httr2 resp_is_error req_url_query
+#' @importFrom httr2 resp_body_string resp_body_json
 ## usethis namespace: end
 NULL
